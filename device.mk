eccdece3d3bb0770f55e954d3ca651abdacaad50
iozbk <165472984+iozbk@users.noreply.github.com>
2026-05-05 16:57:43 +0800

    Update device.mk
    废弃参数

--- device.mk
+++ device.mk
@@ -21,12 +21,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.pineapple
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.pineapple \
-    libgptutils \
-    libz \
-    libcutils
-
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
